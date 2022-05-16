86b734f6c67fa1bb765b5a0468db1d2ff0bfd5cf
IngridAkeida <ingridsanchesp@gmail.com>
2022-05-16 16:31:31 +0200

    update commit validacaoComMensagem

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 console.log('Javascript laralara');
 
+function validaCPF(cpf){
+    return true;
+}
+
 
 function validacao(){
     console.log('Iniciando validacao de cpf');
 
-    
+
     var cpf = document.getElementById('cpf_digitado').value;
+
+    var resultadovalidacao = validaCPF(cpf);
+
+    if(resultadovalidacao){
+        document.getElementById('success').style.display = 'block';
+    }
 
     console.log(cpf);
 }
